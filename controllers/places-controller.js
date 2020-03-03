222c1dce9f57c55b2419b0a63be09ceb6698af72
Kenneth <sally@sallyde-MacBook-Air.local>
2020-03-03 00:54:17 -0800

    added validation so that user cannot enter empty input to certain input fields

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,5 +1,6 @@
 const HttpError = require("../models/http-error");
 const uuid = require("uuid/v4");
+const { validationResult } = require("express-validator");
 
 let DUMMY_PLACES = [
   {
@@ -43,6 +44,11 @@ const getPlacesByUserId = (req, res, next) => {
 };
 
 const createPlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError("Invalid inputs passed, please check your data", 422);
+  }
   const { title, description, coordinates, address, creator } = req.body;
   //const title = req.body.title
   const createdPlace = {
@@ -60,6 +66,11 @@ const createPlace = (req, res, next) => {
 }; //post request has a req.body
 
 const updatePlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError("Invalid inputs passed, please check your data", 422);
+  }
   const { title, description } = req.body;
   const placeId = req.params.pid;
   const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) }; // this will copy and create a new obj of place
@@ -74,6 +85,9 @@ const updatePlace = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
+  if (!DUMMY_PLACES.find(p => p.id === placeId)) {
+    throw new HttpError("Could not find a place for that id", 404);
+  }
   DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
   res.status(200).json({ message: "deleted places" });
 };
--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,5 +1,6 @@
 const uuid = require("uuid/v4");
 const HttpError = require("../models/http-error");
+const { validationResult } = require("express-validator");
 
 DUMMY_USERS = [
   {
@@ -19,11 +20,16 @@ const getUsers = (req, res, next) => {
 };
 
 const signup = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError("Invalid inputs passed, please check your data", 422);
+  }
   const { name, email, password } = req.body;
 
   const hasUser = DUMMY_USERS.find(u => u.email === email);
-  if(hasUser) {
-    throw new HttpError("Could not create user, email already exists.", 422)
+  if (hasUser) {
+    throw new HttpError("Could not create user, email already exists.", 422);
   }
 
   const createdUser = {
